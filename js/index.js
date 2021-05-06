096f99a33c379229276b9bb59bc2158c41314d2f
Luna Tauer <lunatauer@Lauras-Air.lan>
2021-05-05 19:18:27 -0700

    main content

--- js/index.js
+++ js/index.js
@@ -59,3 +59,14 @@ ctaButton.textContent = siteContent['cta']['button']
 
 let ctaImage = document.querySelector('#cta-img');
 ctaImage.src = siteContent.cta['img-src'];
+
+const h4 = document.querySelectorAll('.text-content');
+h4[0].textContent = siteContent['main-content']['features-h4'];
+h4[1].textContent = siteContent['main-content']['about-h4'];
+h4[2].textContent = siteContent['main-content']['services-h4'];
+h4[3].textContent = siteContent['main-content']['product-h4'];
+h4[4].textContent = siteContent['main-content']['vision-h4'];
+
+let middleImage = document.getElementById('middle-img');
+middleImage.src = siteContent['main-content']['middle-img-src'];
+
